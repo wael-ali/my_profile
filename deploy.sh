14eb80dd147cc53009dc3b0b13cd68271d3b981f
Wael Ali <wael.ali@elbformat.de>
2021-06-24 20:04:21 +0200

    github pages configs_3

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wael-ali/my_profile.git main:gh-pages
+git push -f git@github.com:wael-ali/my_profile.git master:gh-pages
 
 cd -
