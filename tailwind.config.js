5ff196e8e231dd852ca81e86b8a4ef7d599ffe7c
Wael Ali <wael.ali@elbformat.de>
2021-06-25 11:43:37 +0200

    add custom background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#36393f',
+        secondary: '#2f3136',
+        teritary: '#202225'
+      }
+    },
   },
   variants: {
     extend: {},
