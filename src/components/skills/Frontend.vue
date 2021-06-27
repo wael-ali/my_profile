4da332ba9ef8d5526a14123b944c44e07d45e83a
Wael Ali <wael.ali@elbformat.de>
2021-06-27 22:23:19 +0200

    add front-end skills

--- src/components/skills/Frontend.vue
+++ src/components/skills/Frontend.vue
@@ -1,9 +1,42 @@
 <template>
-  <div class="w-full h-full flex place-items-center">
-    <div class="ml-20">
-      <h1 class="mb-10 text-7xl font-semibold leading-tight italic">
-        Frontend page comming soon!!
-      </h1>
+  <div class="w-full h-full">
+    <div class="p-20 font-semibold tracking-widest italic">
+      <div class="mb-10">
+        <div>Vuejs</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-10/12 bg-green-600 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>HTML</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-10/12 bg-pink-600 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>CSS</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-8/12 bg-yellow-600 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>Tailwind</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-5/12 bg-purple-600 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>React</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-6/12 bg-red-500 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>Bootstrap</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-8/12 bg-blue-500 h-full"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
