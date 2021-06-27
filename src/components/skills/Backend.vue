138b45dd76432822fcf35e57881c1a4c2bfba74f
Wael Ali <wael.ali@elbformat.de>
2021-06-27 22:23:01 +0200

    add backend-skills

--- src/components/skills/Backend.vue
+++ src/components/skills/Backend.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="w-full h-full">
-    <div class="">
-      <div class="p-20">
-        <div class="mb-10">
-          <div>Express</div>
-          <div class="w-full h-1 bg-gray-700">
-            <div class="w-9/12 bg-purple-600 h-full"></div>
-          </div>
+    <div class="p-20 font-semibold tracking-widest italic">
+      <div class="mb-10">
+        <div>Express</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-9/12 bg-purple-600 h-full"></div>
         </div>
-        <div class="mb-10">
-          <div>Symfony</div>
-          <div class="w-full h-1 bg-gray-700">
-            <div class="w-9/12 bg-yellow-600 h-full"></div>
-          </div>
+      </div>
+      <div class="mb-10">
+        <div>Symfony</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-10/12 bg-yellow-600 h-full"></div>
         </div>
-        <div class="mb-10">
-          <div>MySQL</div>
-          <div class="w-full h-1 bg-gray-700">
-            <div class="w-9/12 bg-yellow-300 h-full"></div>
-          </div>
+      </div>
+      <div class="mb-10">
+        <div>MySQL</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-9/12 bg-yellow-300 h-full"></div>
         </div>
-        <div class="mb-10">
-          <div>MongoDB</div>
-          <div class="w-full h-1 bg-gray-700">
-            <div class="w-5/12 bg-red-700 h-full"></div>
-          </div>
+      </div>
+      <div class="mb-10">
+        <div>MongoDB</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-5/12 bg-green-700 h-full"></div>
         </div>
-        <div class="mb-10">
-          <div>Rails</div>
-          <div class="w-full h-1 bg-gray-700">
-            <div class="w-4/12 bg-red-900 h-full"></div>
-          </div>
+      </div>
+      <div class="mb-10">
+        <div>Rails</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-4/12 bg-red-900 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>Docker</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-5/12 bg-blue-600 h-full"></div>
         </div>
       </div>
     </div>
