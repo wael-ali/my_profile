be03ca5e99889cc1f076f5c593a6bb24e3d7133e
Wael Ali <wael.ali@elbformat.de>
2021-06-27 22:23:30 +0200

    add other skills

--- src/components/skills/Other.vue
+++ src/components/skills/Other.vue
@@ -1,9 +1,42 @@
 <template>
-  <div class="w-full h-full flex place-items-center">
-    <div class="ml-20">
-      <h1 class="mb-10 text-7xl font-semibold leading-tight italic">
-        Other Skills page comming soon!!
-      </h1>
+  <div class="w-full h-full">
+    <div class="p-20 font-semibold tracking-widest italic">
+      <div class="mb-10">
+        <div>JS/NODE</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-10/12 bg-pink-600 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>PHP</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-10/12 bg-green-600 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>GIT</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-6/12 bg-red-500 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>Ruby</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-5/12 bg-yellow-600 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>GraphQl</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-6/12 bg-blue-500 h-full"></div>
+        </div>
+      </div>
+      <div class="mb-10">
+        <div>Java</div>
+        <div class="w-full h-1 bg-gray-700">
+          <div class="w-6/12 bg-pink-900 h-full"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
